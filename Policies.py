d26bb05d41c73fed7df642ac680b2d03d1884c6f
Jonathan Walker <jonathan.walker@sas.com>
2017-11-30 17:12:01 -0500

    e-greedy add

--- Policies.py
+++ Policies.py
@@ -13,3 +13,30 @@ class BoltzmannPolicy():
         probs = probs.ravel()
 
         return np.random.choice(range(qvalues.size), p=probs)
+
+
+class EpsilonGreedyPolicy():
+    def __init__(self, max=1.0, min=0.0, decay=0.9, exploration_episodes=0):
+        self.epsilon = max
+        self.min = min
+        self.decay = decay
+        self.exploration_episodes = exploration_episodes
+        self.episode_count = 0
+
+    def __call__(self, qvalues, *args, **kwargs):
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice(range(qvalues.size))
+        else:
+            # Get the index (action #s) of the actions with max Q Values
+            best_actions = np.argwhere(qvalues == np.max(qvalues))[:, 1]
+
+            # Randomly choose one of the best actions
+            a = np.random.choice(best_actions.flatten())
+            return a
+
+    def on_episode_complete(self):
+        self.episode_count += 1
+        if self.episode_count > self.exploration_episodes:
+            self.epsilon = max(self.epsilon * self.decay, self.min)
+        print('Episode: {}  Epsilon: {}'.format(self.episode_count, self.epsilon))
+        # TODO: Add to logger
