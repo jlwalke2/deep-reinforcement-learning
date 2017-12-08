1e70e6d08b5b9a19f47fb885dd895d2ccba22b27
jlwalke2 <jlwalker@inbox.com>
2017-12-08 13:26:43 -0500

    rough draft of evolution strategy agent

--- Agents.py
+++ Agents.py
@@ -20,7 +20,8 @@ class Monitor(logging.getLoggerClass()):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.episode_metrics = []
-        self.recent_rewards = deque(maxlen=50)
+        self.recent_rewards = deque(maxlen=100)
+        self.episode_start_time = None
 
     def on_episode_start(self, *args, **kwargs):
         # Save start time so we can calculate episode duration later
@@ -32,7 +33,11 @@ class Monitor(logging.getLoggerClass()):
         self.recent_rewards.append(kwargs['total_reward'])
         avg_reward = sum(self.recent_rewards) / len(self.recent_rewards)
 
-        episode_duration = datetime.now() - self.episode_start_time
+        if self.episode_start_time:
+            episode_duration = datetime.now() - self.episode_start_time
+        else:
+            self.warning('Episode Start Time is unknown.  Has on_episode_start been called?')
+            episode_duration = 0
 
         self.info('Episode {}: \tError: {},\tReward: {} \tMoving Avg Reward:{}\tSteps: {}\tDuration: {}'.format(
             round(kwargs['episode_count'], 2),
@@ -98,9 +103,13 @@ class Agent:
         self.step_end = EventHandler()
         self.train_start = EventHandler()
         self.train_end = EventHandler()
+        self.calculate_error = EventHandler()       # Called as pre-process hook for error calculation
+
+        # TODO: differentiate callbacks from preprocess steps
 
         # Automatically hook up any events
-        for event in ['on_episode_start', 'on_episode_end', 'on_step_start', 'on_step_end', 'on_train_start', 'on_train_end']:
+        for event in ['on_episode_start', 'on_episode_end', 'on_step_start', 'on_step_end', 'on_train_start',
+                      'on_train_end', 'on_calculate_error']:
             handler = event.replace('on_', '')
 
             if handler in dir(self):
@@ -131,7 +140,7 @@ class DeepQAgent(Agent):
         self.preprocess_steps = []
 
 
-    def train(self, max_episodes=500, steps_before_training=None, target_model_update=1000, render_every_n=1, upload=False, running_average_len=50):
+    def train(self, max_episodes=500, steps_before_training=None, target_model_update=1000, render_every_n=1, upload=False):
         if upload:
             assert self.api_key, 'An API key must be specified before uploading training results.'
             monitor_path = mkdtemp()
@@ -180,7 +189,6 @@ class DoubleDeepQAgent(DeepQAgent):
 
         s = self.env.reset()  # Get initial state observation
         
-        # TODO: Terminate after max steps
         while not episode_done:
             if render:
                 self.env.render()
@@ -270,13 +278,144 @@ class DoubleDeepQAgent(DeepQAgent):
         # Compute the new Q value of s
         updated_targets = rewards + self.gamma * best_values.reshape(rewards.shape)
 
-        targets = self.model.predict_on_batch(states)
-        # TODO: Add callback w/ diff for priority replay
-        diff = targets[range(n), actions.astype('int32').ravel()] - updated_targets[:, 0]
+        # Target and estimated q-values should be same for any action not taken from s
+        # This ensures the difference between targte & estimated q-values is 0 and only the selected action influences
+        # training.
+        estimate = self.model.predict_on_batch(states)
+        targets = estimate.copy()
         targets[range(n), actions.astype('int32').ravel()] = updated_targets[:, 0]
+
+        self.calculate_error(target=targets, estimate=estimate)
+
+        #targets = self.model.predict_on_batch(states)
+        # TODO: Add callback w/ diff for priority replay
+
+        diff = targets[range(n), actions.astype('int32').ravel()] - updated_targets[:, 0]
+        #targets[range(n), actions.astype('int32').ravel()] = updated_targets[:, 0]
 
         error = self.model.train_on_batch(states, targets)
 
         self.train_end(num_samples=n, s=states, a=actions, r=rewards, s_prime=s_primes, episode_done=flags, target=targets, delta=diff, error=error)
 
         return error
+
+
+class EvolutionStrategyAgent(Agent):
+    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _update_weights(self, epsilon):
+        pass
+
+    def train(self):
+        sigma = 0.1 # Pass as input?  Constructor?
+        alpha = 1e-3
+        epsilons = []
+        rewards = []
+        n = 100  # Population size (& # of episodes per training update)
+        epochs = 20
+
+        for epoch in range(epochs):
+
+            starting_weights = self.model.get_weights()
+            t1 = starting_weights[0] + 1.
+
+            for i in range(n): # # population size
+                epsilon = []
+                new_weights = []
+                for w in starting_weights:
+                    e = np.random.randn(*w.shape)
+                    new_weights.append(w + sigma * e)
+                    epsilon.append(e)
+                self.model.set_weights(new_weights)
+
+                self.episode_start(episode_count=epoch)
+
+                # Run episode
+                total_reward, total_error, step_count = self._run_episode(0, True)
+
+                # Fire any notifications
+                self.episode_end(episode_count=epoch, total_reward=total_reward, total_error=total_error, num_steps=step_count)
+
+                epsilons.append(epsilon)
+                rewards.append(total_reward)
+
+            new_weights = []
+            for layer in range(len(starting_weights)):
+                t0 = [rewards[i] * epsilons[i][layer] for i in range(n)]
+                t1 = sum(t0)
+                t2 = alpha * 1. / (n * sigma) * t1
+                new_weights.append(starting_weights[layer] + t2)
+
+            self.model.set_weights(new_weights)
+
+
+
+
+
+
+    def _run_episode(self, total_steps, render):
+            episode_done = False
+            total_reward = 0
+            total_error = 0
+            step_count = 0
+
+            s = self.env.reset()  # Get initial state observation
+
+            while not episode_done:
+                if render:
+                    self.env.render()
+                s = np.asarray(s)
+
+                self.step_start(step=step_count, total_steps=total_steps, s=s)
+
+                q_values = self.model.predict_on_batch(s.reshape(1, -1))
+                a = self.policy(q_values)
+
+                s_prime, r, episode_done, _ = self.env.step(a)
+
+                step_count += 1
+                total_steps += 1
+                total_reward += r  # Track rewards without shaping
+
+                s, a, r, s_prime, episode_done = self.preprocess_state(s, a, r, s_prime, episode_done)
+
+                # Force the episode to end if we've reached the maximum number of steps allowed
+                if self.max_steps_per_episode and step_count >= self.max_steps_per_episode:
+                    episode_done = True
+
+                self.step_end(step=step_count, total_steps=total_steps, s=s, s_prime=s_prime, a=a, r=r,
+                              episode_done=episode_done)
+                self.logger.debug("S: {}\tA: {}\tR: {}\tS': {}\tDone: {}".format(s, a, r, s_prime, episode_done))
+
+                s = s_prime
+
+            return total_reward, total_error, step_count
+
+            # Sample noise
+            # Update weights
+            # run episode
+            # reverse weight change
+            # average returns and
+
+
+
+
+
+    '''
+    train
+    run episode
+    update weights
+
+
+    for each episode, sample random noise
+    (temp) update weights
+    run episode
+    get total reward
+    compute gradient
+    actual weight update
+
+    batch normalization
+    '''
