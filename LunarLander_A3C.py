002f2622d426f4106856ab8ee2066bc62c426a63
jlwalke2 <jlwalker@inbox.com>
2017-12-30 21:33:02 -0500

    Initial commit

---
+++ LunarLander_A3C.py
@@ -0,0 +1,41 @@
+import gym
+from deeprl.agents.A3CAgent import A3CAgent
+import keras.backend as K
+from keras.layers import Dense, Flatten, Input, concatenate
+from keras.models import Sequential, Model
+from keras.optimizers import rmsprop
+
+if __name__ == '__main__':
+    env = gym.make('LunarLander-v2')
+
+    num_features = env.observation_space.shape[0]
+    num_actions = env.action_space.n
+
+    def actor_loss(y_true, y_pred):
+        return K.mean(y_true - y_pred, axis=-1)
+
+    actor = Sequential([
+        Dense(16, input_dim=num_features, activation='relu'),
+        Dense(16, activation='relu'),
+        Dense(units=num_actions, activation='softmax')
+    ])
+    actor.compile(loss=actor_loss, optimizer=rmsprop(lr=0.0016, decay=0.000001))
+
+
+    critic = Sequential([
+        Dense(16, input_dim=num_features, activation='relu'),
+        Dense(16, activation='relu'),
+        Dense(units=1, activation='linear')
+    ])
+    critic.compile(loss='mse', optimizer=rmsprop(lr=0.0016, decay=0.000001))
+
+    agent = A3CAgent(env=env, actor=actor, critic=critic, max_steps_per_episode=500)
+    import logging
+    agent.logger.setLevel(logging.DEBUG)
+    agent.train(num_threads=1, max_steps=10, render_every_n=25)
+
+    df = agent.logger.get_episode_metrics()
+    p = df.plot.line(x='episode_count', y='total_reward')
+    p = df.plot.line(x='episode_count', y='mean_reward', ax=p)
+    p.figure.show()
+    pass
