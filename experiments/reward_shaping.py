cdd1acab4a41c4a585ccd2da6ca193b41e997bdd
jlwalke2 <jlwalker@inbox.com>
2018-01-10 07:21:39 -0500

    save and plot metrics

--- experiments/reward_shaping.py
+++ experiments/reward_shaping.py
@@ -9,7 +9,10 @@ import gym
 import multiprocessing
 import logging
 
-def run_agent_no_shaping(name, metrics):
+num_agents = 3
+num_episodes = 1000
+
+def run_agent_no_shaping(metrics):
     env = gym.make('LunarLander-v2')
 
     num_features = DoubleDeepQAgent._get_space_size(env.observation_space)
@@ -24,15 +27,15 @@ def run_agent_no_shaping(name, metrics):
     logger = multiprocessing.log_to_stderr()
     logger.setLevel(logging.INFO)
 
-    agent = DoubleDeepQAgent(name=name, env=env, model=model,
+    agent = DoubleDeepQAgent(name=multiprocessing.current_process().name, env=env, model=model,
                              policy=EpsilonGreedyPolicy(min=0.025, decay=0.96, exploration_episodes=1),
                              memory=PrioritizedMemory(maxlen=50000, sample_size=32),
                              logger=logger, metrics=metrics, gamma=0.99, max_steps_per_episode=500)
 
-    agent.train(target_model_update=750, max_episodes=10, render_every_n=10)
+    agent.train(target_model_update=750, max_episodes=num_episodes, render_every_n=num_episodes+1)
 
 
-def run_agent_with_shaping(name, metrics):
+def run_agent_with_shaping(metrics):
     env = gym.make('LunarLander-v2')
 
     num_features = DoubleDeepQAgent._get_space_size(env.observation_space)
@@ -47,12 +50,23 @@ def run_agent_with_shaping(name, metrics):
     logger = multiprocessing.log_to_stderr()
     logger.setLevel(logging.INFO)
 
-    agent = DoubleDeepQAgent(name=name, env=env, model=model,
+    def shape_reward(*args):
+        s, a, r, s_prime, done = args
+        def potential(state):
+            x_pos = state[0]
+            y_pos = state[1]
+            return (1 - abs(x_pos)) + (1 - y_pos)  # Encourage moving to 0,0 (center of landing pad)
+
+        r = r + 0.99 * potential(s_prime) - potential(s)
+        return args
+
+    agent = DoubleDeepQAgent(name=multiprocessing.current_process().name, env=env, model=model,
                              policy=EpsilonGreedyPolicy(min=0.025, decay=0.96, exploration_episodes=1),
                              memory=PrioritizedMemory(maxlen=50000, sample_size=32),
                              logger=logger, metrics=metrics, gamma=0.99, max_steps_per_episode=500)
+    agent.preprocess_state = shape_reward
 
-    agent.train(target_model_update=750, max_episodes=10, render_every_n=10)
+    agent.train(target_model_update=750, max_episodes=num_episodes, render_every_n=num_episodes+1)
 
 
 if __name__ == '__main__':
@@ -60,8 +74,8 @@ if __name__ == '__main__':
     manager.start()
     metrics = manager.Monitor()
 
-    processes = [multiprocessing.Process(target=run_agent_no_shaping, args=('AgentNoShape', metrics), name='Worker 1'),
-                 multiprocessing.Process(target=run_agent_with_shaping, args=('AgentWithShape', metrics), name='Worker 2')]
+    processes = [multiprocessing.Process(target=run_agent_no_shaping, args=(metrics,), name='AgentNoShape_{}'.format(i)) for i in range(num_agents)]
+    processes.extend([multiprocessing.Process(target=run_agent_with_shaping, args=(metrics,), name='AgentWithShape_{}'.format(i)) for i in range(num_agents)])
 
     for p in processes:
         p.start()
@@ -70,12 +84,5 @@ if __name__ == '__main__':
         p.join()
 
     metrics.save('reward_shaping.csv')
-    df = metrics.get_episode_metrics()
-    df.plot()
-    pass
-
-
-# multiple agents
-# multiple run funcs
-# multiprocessing
+    metrics.get_episode_metrics().pivot(columns='sender', values='avg_reward').plot().figure.savefig('reward_shaping_avg_reward.png')
 
