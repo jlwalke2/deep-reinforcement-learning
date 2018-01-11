f32f39fba725ca438a897b0571f5455283eb1ebf
jlwalke2 <jlwalker@inbox.com>
2018-01-11 16:45:02 -0500

    fixed imports added logger

--- deeprl/agents/A3CAgent.py
+++ deeprl/agents/A3CAgent.py
@@ -1,6 +1,6 @@
 from .AbstractAgent import AbstractAgent
 from ..utils.async import ModelManager
-from Memories import TrajectoryMemory
+from deeprl.memories import TrajectoryMemory
 import gym
 import multiprocessing
 from multiprocessing import Process
@@ -8,32 +8,44 @@ import numpy as np
 import keras.backend as K
 import keras.models
 
-def run_worker(id, config, actor, critic):
-    worker = A3CWorker(id, config, actor, critic)
+
+def run_worker(id, config, actor, critic, logger=None, metrics=None):
+    worker = A3CWorker(id, config, actor, critic, logger)
     worker.train()
 
 
-class A3CWorker():
+class A3CWorker(AbstractAgent):
     # TODO: Implement shared logger
     # TODO: Pass gamma in config
     # TODO: Allow shared weights between actor & critic
     # TODO: Support id/name in metrics collection
 
-    def __init__(self, id, shared_config, shared_actor, shared_critic):
+    def __init__(self, id, shared_config, shared_actor, shared_critic, logger=None, metrics=None):
         assert 'env' in shared_config, 'Configuration missing key "env".'
 
         self.id = id
         self.name = multiprocessing.current_process().name
         self.config = shared_config
-        self.env = gym.make(self.config['env'])
+
         self.local_actor = self._build_model(shared_actor, lambda y_true, y_pred: y_pred)
         self.local_critic = self._build_model(shared_critic, 'mse')
         self.shared_actor = shared_actor
         self.shared_critic = shared_critic
-        self.num_actions = self.env.action_space.n
-        self.train_actor_on_batch = self._build_actor_train_func(self.local_actor)
 
-        print(self.name)
+        self.train_actor_on_batch = self._build_actor_train_func(self.local_actor)
+        self.gamma = self.config['gamma']
+        self.train_interval = 5                 # TODO: Pull from config or default
+
+        env = gym.make(self.config['env'])
+        memory = TrajectoryMemory(maxlen=self.train_interval)
+        max_steps = self.config['max_steps_per_episode']
+        logger = logger or multiprocessing.log_to_stderr()
+
+        super(A3CWorker, self).__init__(env, memory=memory, max_steps_per_episode=max_steps, logger=logger)
+
+
+
+        self.logger.info('Starting {}...'.format(self.name))
 
 
     def _build_actor_train_func(self, model):
@@ -79,15 +91,11 @@ class A3CWorker():
 
     def train(self):
         max_episodes = self.config['max_episodes']
-        train_interval = 5
-        max_steps = self.config['max_steps_per_episode']
-        gamma = self.config['gamma']
-        memory = TrajectoryMemory(maxlen=train_interval)
 
         # Construct an upper triangular matrix where each diagonal 0..k = the discount factor raised to that power
         # Once constructed, the n-step return can be computed as Gr where G is the matrix of discount factors
         # and r is the vector of rewards observed at each time step
-        G = sum([np.diagflat(np.ones(train_interval - i) * gamma ** i, k=i) for i in range(train_interval)])
+        G = sum([np.diagflat(np.ones(self.train_interval - i) * self.gamma ** i, k=i) for i in range(self.train_interval)])
 
         for episode in range(max_episodes):
             orig_actor_weights = self.shared_actor.get_weights()  # Refresh local copies of weights
@@ -107,16 +115,16 @@ class A3CWorker():
                 s_prime, r, done, _ = self.env.step(a)
 
                 # Store experience
-                memory.append((s, a, r, s_prime, done))
+                self.memory.append((s, a, r, s_prime, done))
                 s = s_prime
 
                 # Terminate loop anyways if max steps reached.
                 # Episode not marked as terminal so V(s') estimate will be used
-                if step == max_steps:
+                if step == self.max_steps_per_episode:
                     done = True
 
-                if done or step % train_interval == 0:
-                    states, actions, rewards, s_primes, flags = memory.sample()
+                if done or step % self.train_interval == 0:
+                    states, actions, rewards, s_primes, flags = self.memory.sample()
                     batch_size = states.shape[0]
 
                     # Total n-step return from each state
@@ -127,8 +135,7 @@ class A3CWorker():
                     # Discount factor for V(s') is gamma * discount for last reward observed before s'.
                     if flags[-1] == False:
                         terminal_val = self.local_critic.predict(s_primes[-1, :].reshape((1, -1)))
-                        R += (G[:, -1] * gamma * terminal_val).reshape(R.shape)
-
+                        R += (G[:, -1] * self.gamma * terminal_val).reshape(R.shape)
 
                     # Train the actor network
                     mask = np.zeros((batch_size, self.num_actions))
@@ -140,7 +147,7 @@ class A3CWorker():
                     # Train the critic network
                     critic_err = self.local_critic.train_on_batch(states[:-1, :], R[:-1])
 
-                    print('{} | episode = {},  step = {},  actor error = {},  critic error = {}'.format(self.name, episode, step, '', critic_err))
+                    self.logger.info('{} | episode = {},  step = {},  actor error = {},  critic error = {}'.format(self.name, episode, step, '', critic_err))
 
                     # Update the shared model weights and refresh local weights
                     actor_delta = [new - old for new, old in zip(self.local_actor.get_weights(), orig_actor_weights)]
@@ -151,9 +158,6 @@ class A3CWorker():
                     orig_critic_weights = self.shared_critic.add_delta(critic_delta)  # Add deltas and get updated weights
                     self.local_critic.set_weights(orig_critic_weights)  # Update the model with new weights
 
-        pass
-
-
 
 
 
@@ -162,7 +166,9 @@ class A3CWorker():
 class A3CAgent(AbstractAgent):
     def __init__(self, actor, critic, **kwargs):
 
-        kwargs['model'] = actor
+        if 'logger' not in kwargs:
+            kwargs['logger'] = multiprocessing.log_to_stderr()
+
         super(A3CAgent, self).__init__(**kwargs)
 
         self.actor = actor
@@ -184,6 +190,7 @@ class A3CAgent(AbstractAgent):
 
         actor = self._build_shared_model(self.actor, manager)
         critic = self._build_shared_model(self.critic, manager)
+        metrics = manager.Monitor()
 
         config = manager.dict()
         config['env'] = self.env.spec.id
