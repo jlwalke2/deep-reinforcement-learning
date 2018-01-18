356d5d5397ca6ff2194f4a5d874369b644d883aa
jlwalke2 <jlwalker@inbox.com>
2018-01-18 15:46:52 -0500

    switched to MountainCar

--- experiments/reward_shaping.py
+++ experiments/reward_shaping.py
@@ -5,80 +5,85 @@ from deeprl.utils.async import ModelManager
 from deeprl.utils.metrics import *
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import rmsprop
+from keras.optimizers import adam
 import gym
 import multiprocessing
 import logging
+import numpy as np
 
 num_agents = 5
 num_episodes = 1000
 
 def run_agent_no_shaping(history):
-    env = gym.make('LunarLander-v2')
+    env = gym.make('MountainCar-v0')
 
     num_features = DoubleDeepQAgent._get_space_size(env.observation_space)
     num_actions = DoubleDeepQAgent._get_space_size(env.action_space)
 
-    model = Sequential([
-        Dense(64, input_dim=num_features, activation='relu'),
-        Dense(64, activation='relu'),
-        Dense(units=num_actions, activation='linear')
-    ])
-    model.compile(loss='mse', optimizer=rmsprop(lr=0.0016, decay=0.000001))
+    model = Sequential()
+    model.add(Dense(16, input_dim=num_features, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(units=num_actions, activation='linear'))
+    model.compile(loss='mse', optimizer=adam())
+
     logger = multiprocessing.log_to_stderr()
     logger.setLevel(logging.INFO)
 
     agent = DoubleDeepQAgent(name=multiprocessing.current_process().name, env=env, model=model,
-                             policy=EpsilonGreedyPolicy(min=0.025, decay=0.96, exploration_episodes=1),
-                             memory=PrioritizedMemory(maxlen=50000, sample_size=32),
+                             policy=EpsilonGreedyPolicy(min=0.05, max=0.5, decay=0.999),
+                             memory=PrioritizedMemory(maxlen=50000),
                              logger=logger, history=history,
-                             metrics=[EpisodeReward(), RollingEpisodeReward(), CumulativeReward()],
-                             gamma=0.99, max_steps_per_episode=500)
+                             metrics=[EpisodeReward(), RollingEpisodeReward(), CumulativeReward(), EpisodeTime()],
+                             gamma=0.99, max_steps_per_episode=1000)
 
-    agent.train(target_model_update=750, max_episodes=num_episodes, render_every_n=num_episodes+1)
+    agent.train(target_model_update=1e-2, max_episodes=num_episodes, render_every_n=None)
 
 
 def run_agent_with_shaping(history):
-    env = gym.make('LunarLander-v2')
+    env = gym.make('MountainCar-v0')
 
     num_features = DoubleDeepQAgent._get_space_size(env.observation_space)
     num_actions = DoubleDeepQAgent._get_space_size(env.action_space)
 
-    model = Sequential([
-        Dense(64, input_dim=num_features, activation='relu'),
-        Dense(64, activation='relu'),
-        Dense(units=num_actions, activation='linear')
-    ])
-    model.compile(loss='mse', optimizer=rmsprop(lr=0.0016, decay=0.000001))
+    def shape_reward(*args):
+        # state is (position, velocity)
+        s, a, r, s_prime, done = args
+
+        def potential(state):
+            return 1. if np.all(state > 0) or np.all(state < 0) else 0
+
+        r = r + 0.99 * potential(s_prime) - potential(s)
+
+        return args
+
+    model = Sequential()
+    model.add(Dense(16, input_dim=num_features, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(units=num_actions, activation='linear'))
+    model.compile(loss='mse', optimizer=adam())
+
     logger = multiprocessing.log_to_stderr()
     logger.setLevel(logging.INFO)
 
-    def shape_reward(*args):
-        s, a, r, s_prime, done = args
-        def potential(state):
-            x_pos = state[0]
-            y_pos = state[1]
-            return (1 - abs(x_pos)) + (1 - y_pos)  # Encourage moving to 0,0 (center of landing pad)
-
-        r = r + 0.99 * potential(s_prime) - potential(s)
-        return (s, a, r, s_prime, done)
-
     agent = DoubleDeepQAgent(name=multiprocessing.current_process().name, env=env, model=model,
-                             policy=EpsilonGreedyPolicy(min=0.025, decay=0.96, exploration_episodes=1),
-                             memory=PrioritizedMemory(maxlen=50000, sample_size=32),
+                             policy=EpsilonGreedyPolicy(min=0.05, max=0.5, decay=0.999),
+                             memory=PrioritizedMemory(maxlen=50000),
                              logger=logger, history=history,
-                             metrics=[EpisodeReward(), RollingEpisodeReward(), CumulativeReward()],
-                             gamma=0.99, max_steps_per_episode=500)
+                             metrics=[EpisodeReward(), RollingEpisodeReward(), CumulativeReward(), EpisodeTime()],
+                             gamma=0.99, max_steps_per_episode=1000)
     agent.preprocess_state = shape_reward
 
-    agent.train(target_model_update=750, max_episodes=num_episodes, render_every_n=num_episodes+1)
+    agent.train(target_model_update=1e-2, max_episodes=num_episodes, render_every_n=None)
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import os.path
     from warnings import warn
-    fname = 'reward_shaping.csv'
+    fname = 'reward_shaping.h5'
+
     if not os.path.isfile(fname):
         manager = ModelManager()
         manager.start()
@@ -93,14 +98,23 @@ if __name__ == '__main__':
         for p in processes:
             p.join()
 
-        metrics.save('reward_shaping.csv')
+        metrics.save(fname)
         df = metrics.get_episode_metrics()
     else:
         import pandas as pd
-        warn('Existing history found.  Loading history from {}'.format(fname))
-        df = pd.read_csv(fname)
+        warn('Existing history found.  Loading history from {}'.format(fname), stacklevel=2)
+        from deeprl.utils import History
+        df, _ =  History().load(fname)
         df.set_index('episode', inplace=True)
 
+        # steps_df = pd.read_csv('step_metrics.csv')
+        # steps_df.set_index('step', inplace=True)
+
+    # steps_df.pivot(columns='sender', values='CumulativeReward').groupby(
+    #     lambda colname: 'WithShape' if 'WithShape' in colname else 'NoShape', axis=1).plot()
+    # plt.show()
+    df.pivot(columns='sender', values='CumulativeReward').plot()
+    plt.show()
 
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8,4))
 
