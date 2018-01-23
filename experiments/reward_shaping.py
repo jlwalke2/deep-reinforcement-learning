d20d32421164e629655c98625dd29df59936e8dd
jlwalke2 <jlwalker@inbox.com>
2018-01-23 18:59:48 -0500

    use Experiment class

--- experiments/reward_shaping.py
+++ experiments/reward_shaping.py
@@ -1,20 +1,17 @@
 from deeprl.agents.DoubleDeepQAgent import DoubleDeepQAgent
 from deeprl.memories import PrioritizedMemory
 from deeprl.policies import EpsilonGreedyPolicy
-from deeprl.utils.async import ModelManager
 from deeprl.utils.metrics import *
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import adam
 import gym
-import multiprocessing
-import logging
 import numpy as np
 
-num_agents = 5
-num_episodes = 1000
 
-def run_agent_no_shaping(history):
+num_episodes = 10
+
+def build_agent_no_shaping():
     env = gym.make('MountainCar-v0')
 
     num_features = DoubleDeepQAgent._get_space_size(env.observation_space)
@@ -27,35 +24,35 @@ def run_agent_no_shaping(history):
     model.add(Dense(units=num_actions, activation='linear'))
     model.compile(loss='mse', optimizer=adam())
 
-    logger = multiprocessing.log_to_stderr()
-    logger.setLevel(logging.INFO)
-
-    agent = DoubleDeepQAgent(name=multiprocessing.current_process().name, env=env, model=model,
+    agent = DoubleDeepQAgent(name='AgentNoShaping', env=env, model=model,
                              policy=EpsilonGreedyPolicy(min=0.05, max=0.5, decay=0.999),
                              memory=PrioritizedMemory(maxlen=50000),
-                             logger=logger, history=history,
                              metrics=[EpisodeReward(), RollingEpisodeReward(), CumulativeReward(), EpisodeTime()],
                              gamma=0.99, max_steps_per_episode=1000)
 
+    return agent
+
+def run_agent(agent):
     agent.train(target_model_update=1e-2, max_episodes=num_episodes, render_every_n=None)
 
 
-def run_agent_with_shaping(history):
+def shape_reward(*args):
+    # state is (position, velocity)
+    s, a, r, s_prime, done = args
+
+    def potential(state):
+        return 1. if np.all(state > 0) or np.all(state < 0) else 0
+
+    r = r + 0.99 * potential(s_prime) - potential(s)
+
+    return args
+
+def build_agent_with_shaping():
     env = gym.make('MountainCar-v0')
 
     num_features = DoubleDeepQAgent._get_space_size(env.observation_space)
     num_actions = DoubleDeepQAgent._get_space_size(env.action_space)
 
-    def shape_reward(*args):
-        # state is (position, velocity)
-        s, a, r, s_prime, done = args
-
-        def potential(state):
-            return 1. if np.all(state > 0) or np.all(state < 0) else 0
-
-        r = r + 0.99 * potential(s_prime) - potential(s)
-
-        return args
 
     model = Sequential()
     model.add(Dense(16, input_dim=num_features, activation='relu'))
@@ -64,48 +61,49 @@ def run_agent_with_shaping(history):
     model.add(Dense(units=num_actions, activation='linear'))
     model.compile(loss='mse', optimizer=adam())
 
-    logger = multiprocessing.log_to_stderr()
-    logger.setLevel(logging.INFO)
-
-    agent = DoubleDeepQAgent(name=multiprocessing.current_process().name, env=env, model=model,
+    agent = DoubleDeepQAgent(name='AgentWithShaping', env=env, model=model,
                              policy=EpsilonGreedyPolicy(min=0.05, max=0.5, decay=0.999),
                              memory=PrioritizedMemory(maxlen=50000),
-                             logger=logger, history=history,
                              metrics=[EpisodeReward(), RollingEpisodeReward(), CumulativeReward(), EpisodeTime()],
                              gamma=0.99, max_steps_per_episode=1000)
     agent.preprocess_state = shape_reward
 
-    agent.train(target_model_update=1e-2, max_episodes=num_episodes, render_every_n=None)
+    return agent
 
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    import os.path
-    from warnings import warn
-    fname = 'reward_shaping.h5'
 
-    if not os.path.isfile(fname):
-        manager = ModelManager()
-        manager.start()
-        metrics = manager.Monitor()
+    from experiments.experiment import Experiment
 
-        processes = [multiprocessing.Process(target=run_agent_no_shaping, args=(metrics,), name='AgentNoShape_{}'.format(i)) for i in range(num_agents)]
-        processes.extend([multiprocessing.Process(target=run_agent_with_shaping, args=(metrics,), name='AgentWithShape_{}'.format(i)) for i in range(num_agents)])
+    agents = [(build_agent_no_shaping(), 3, run_agent),
+              (build_agent_with_shaping(), 3, run_agent)]
 
-        for p in processes:
-            p.start()
+    e = Experiment('reward_shaping', agents)
 
-        for p in processes:
-            p.join()
+    e.run()
 
-        metrics.save(fname)
-        df = metrics.get_episode_metrics()
-    else:
-        import pandas as pd
-        warn('Existing history found.  Loading history from {}'.format(fname), stacklevel=2)
-        from deeprl.utils import History
-        df, _ =  History().load(fname)
-        df.set_index('episode', inplace=True)
+    # if not os.path.isfile(fname):
+    #     manager = ModelManager()
+    #     manager.start()
+    #     metrics = manager.Monitor()
+    #
+    #     processes = [multiprocessing.Process(target=run_agent_no_shaping, args=(metrics,), name='AgentNoShape_{}'.format(i)) for i in range(num_agents)]
+    #     processes.extend([multiprocessing.Process(target=run_agent_with_shaping, args=(metrics,), name='AgentWithShape_{}'.format(i)) for i in range(num_agents)])
+    #
+    #     for p in processes:
+    #         p.start()
+    #
+    #     for p in processes:
+    #         p.join()
+    #
+    #     metrics.save(fname)
+    #     df = metrics.get_episode_metrics()
+    # else:
+    #     import pandas as pd
+    #     warn('Existing history found.  Loading history from {}'.format(fname), stacklevel=2)
+    #     from deeprl.utils import History
+    #     df, _ =  History().load(fname)
+    #     df.set_index('episode', inplace=True)
 
         # steps_df = pd.read_csv('step_metrics.csv')
         # steps_df.set_index('step', inplace=True)
@@ -113,34 +111,40 @@ if __name__ == '__main__':
     # steps_df.pivot(columns='sender', values='CumulativeReward').groupby(
     #     lambda colname: 'WithShape' if 'WithShape' in colname else 'NoShape', axis=1).plot()
     # plt.show()
-    df.pivot(columns='sender', values='CumulativeReward').plot()
-    plt.show()
 
+#    df = e.get_plots([])
+
+    # df.pivot(columns='sender', values='CumulativeReward').plot()
+    # plt.show()
+    #
+    import matplotlib.pyplot as plt
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8,4))
 
-    df.pivot(columns='sender', values='CumulativeReward').groupby(
-        lambda colname: 'WithShape' if 'WithShape' in colname else 'NoShape', axis=1).mean().plot(ax=axes[0], title='Mean Cumulative Reward')
-
-    t0 = df.pivot(columns='sender', values='CumulativeReward').groupby(
-        lambda colname: 'WithShape' if 'WithShape' in colname else 'NoShape', axis=1)
-
-    means = t0.mean()
-    mins = t0.min()
-    maxs = t0.max()
-
-    for col in means.columns:
-        axes[0].fill_between(means.index, mins[col], maxs[col], alpha=0.2)
-
-    df.pivot(columns='sender', values='EpisodeReward').groupby(
-        lambda colname: 'WithShape' if 'WithShape' in colname else 'NoShape', axis=1).mean().plot(ax=axes[1], title='Mean Episode Reward')
-
-    df.pivot(columns='sender', values='EpisodeReward').groupby(
-        lambda colname: 'WithShape' if 'WithShape' in colname else 'NoShape', axis=1).mean().rolling(window=50, min_periods=1).mean().plot(ax=axes[1])
+    e.get_plot('CumulativeReward')
+    e.get_plot('EpisodeReward')
 
     plt.show()
-
-
-    fig.savefig('reward_shaping_avg_reward.png', dpi=500, linewidth=1)
+    pass
+    # t0 = df.pivot(columns='sender', values='CumulativeReward').groupby(
+    #     lambda colname: 'WithShape' if 'WithShape' in colname else 'NoShape', axis=1)
+    #
+    # means = t0.mean()
+    # mins = t0.min()
+    # maxs = t0.max()
+    #
+    # for col in means.columns:
+    #     axes[0].fill_between(means.index, mins[col], maxs[col], alpha=0.2)
+    #
+    # df.pivot(columns='sender', values='EpisodeReward').groupby(
+    #     lambda colname: 'WithShape' if 'WithShape' in colname else 'NoShape', axis=1).mean().plot(ax=axes[1], title='Mean Episode Reward')
+    #
+    # df.pivot(columns='sender', values='EpisodeReward').groupby(
+    #     lambda colname: 'WithShape' if 'WithShape' in colname else 'NoShape', axis=1).mean().rolling(window=50, min_periods=1).mean().plot(ax=axes[1])
+    #
+    # plt.show()
+    #
+    #
+    # fig.savefig('reward_shaping_avg_reward.png', dpi=500, linewidth=1)
 
 
 
